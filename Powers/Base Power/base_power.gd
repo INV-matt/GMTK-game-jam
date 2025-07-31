102321d552a6b212a52106dc55c0e9cccd3713cc
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2025-07-31 12:14:43 +0200

    Added functionality for attack power

--- Powers/Base Power/base_power.gd
+++ Powers/Base Power/base_power.gd
@@ -11,23 +11,23 @@ func _get_player() :
   return parent
 
 # Apply the power's passive effects eg. activating a double jump
-func apply_power_passive() :
+func apply_power_passive() -> void :
   var player = _get_player()
   
   if player :
     _power_passive(player)
 
 # Apply the power's on-death effects eg. exploding violently
-func apply_power_death() :
+func apply_power_death() -> void :
   var player = _get_player()
   
   if player :
     _power_death(player)
 
 # Override this one /w passive power effects
-func _power_passive(player: Player) :
+func _power_passive(player: Player) -> void :
   pass
 
 # Override this one /w on death effects
-func _power_death(player: Player) :
+func _power_death(player: Player) -> void :
   pass
