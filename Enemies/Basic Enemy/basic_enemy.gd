3b864ad6870b8b898f323dd2e8d908f09f66e334
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2025-07-31 11:23:21 +0200

    Enemies now do damage to the player

--- Enemies/Basic Enemy/basic_enemy.gd
+++ Enemies/Basic Enemy/basic_enemy.gd
@@ -3,10 +3,11 @@ extends CharacterBody2D
 class_name Enemy
 
 @export var Speed = 100
+@export var DmgAmt = 5
 
 var _horizontalDirection = 1
 
-func _physics_process(delta: float) -> void:
+func _handleMovement(delta: float) :
   velocity.y += get_gravity().y * delta
   
   velocity.x = _horizontalDirection * Speed
@@ -15,5 +16,14 @@ func _physics_process(delta: float) -> void:
   
   move_and_slide()
   
+  # Flip when hitting a wall (AKA we have not moved because we were blocked)
   if last_pos == global_position :
     _horizontalDirection *= -1
+
+func _physics_process(delta: float) -> void:
+  _handleMovement(delta)
+
+func _on_hitbox_body_entered(body: Player) -> void:
+  for i in body.get_children() :
+    if i is HealthComponent :
+      i.doDamage(DmgAmt)
