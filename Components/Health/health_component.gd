aa8e8305d96ed20bca67f86c8c099baeb20158b6
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2025-07-31 11:06:15 +0200

    Added HealthComponent to enemy

--- Components/Health/health_component.gd
+++ Components/Health/health_component.gd
@@ -4,19 +4,29 @@ class_name HealthComponent
 
 @export var MaxHealth = 100
 @export var DestroyOnNoHealth = false
+@export var DisplayHealthBar: ProgressBar
 
 var health: int
 
 func _ready() -> void:
   health = MaxHealth
+  
+  if DisplayHealthBar :
+    DisplayHealthBar.max_value = MaxHealth
+    DisplayHealthBar.value = health
+    DisplayHealthBar.visible = true
 
 signal death
+signal damaged
 
 func doDamage(amt: int) :
   health -= amt
+  DisplayHealthBar.value = health
   
   if health == 0 :
     emit_signal("death")
     
     if DestroyOnNoHealth :
       get_parent().queue_free()
+  else :
+    emit_signal("damaged")
