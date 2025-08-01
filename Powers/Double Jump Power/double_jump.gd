cad4e28c365515b710d51cb79ced44010ef73a2a
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2025-08-01 17:45:10 +0200

    Added ondeath ability for double-jump power

--- Powers/Double Jump Power/double_jump.gd
+++ Powers/Double Jump Power/double_jump.gd
@@ -7,5 +7,20 @@ func _power_passive(player: Player) :
   player.MaxJumps += 1
   p = player
 
+func _power_death(p: Player) :
+  var area = Area2D.new()
+  var shape = CollisionShape2D.new()
+  
+  shape.shape = RectangleShape2D.new()
+  (shape.shape as RectangleShape2D).size = Vector2(50, 20)
+  
+  area.add_child(shape)
+  area.global_position = p.global_position
+  area.collision_layer = 0
+  area.collision_mask = 2
+  area.connect("body_entered", func(x): p._jumpCount = -1; p._jump())
+  
+  get_tree().get_root().add_child(area)
+
 func _exit_tree() -> void:
   p.MaxJumps -= 1
