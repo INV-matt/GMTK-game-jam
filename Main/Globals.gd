5f0ba452b471b594dc1520417e707e7b77f6a042
INV-matt <matteomarini888@gmail.com>
2025-08-01 09:52:30 +0200

    Added GameManager and fixed UI

--- Main/Globals.gd
+++ Main/Globals.gd
@@ -3,10 +3,12 @@ extends Node
 var _player: Player
 var _main: Node2D
 var _currentLevel: int
+var _gameManager: GameManager
 
 func _ready() -> void:
   _main = get_node("../Main") as Node2D
   _player = _main.get_node("%Player") as Player
+  _gameManager = _main.get_node("%GameManager") as GameManager
 
 func getPlayer() -> Player:
   return _player
@@ -19,3 +21,6 @@ func getCurrentLevel() -> int:
 
 func setCurrentLevel(level: int) -> void:
   _currentLevel = level
+
+func getGameManager() -> GameManager:
+  return _gameManager
