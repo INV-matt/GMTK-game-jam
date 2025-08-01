f8df3ab77bc1394773a512ff9dba10cc0c6a09a7
INV-matt <matteomarini888@gmail.com>
2025-08-01 08:55:16 +0200

    Added Globals

---
+++ Main/Globals.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var _player: Player
+var _main: Node2D
+var _currentLevel: int
+
+func _ready() -> void:
+  _main = get_node("../Main") as Node2D
+  _player = _main.get_node("%Player") as Player
+
+func getPlayer() -> Player:
+  return _player
+
+func getMain() -> Node2D:
+  return _main
+
+func getCurrentLevel() -> int:
+  return _currentLevel
+
+func setCurrentLevel(level: int) -> void:
+  _currentLevel = level
--- Main/LevelManager.gd
+++ Main/LevelManager.gd
@@ -8,28 +8,29 @@ extends Node
 
 var currLevel: int
 
-func _nextLevel() :
+func _nextLevel():
   currLevel += 1
+  Globals.setCurrentLevel(currLevel)
   _loadLevel()
 
-func _loadLevel() :
-  if currLevel >= len(levels) :
+func _loadLevel():
+  if currLevel >= len(levels):
     return
     
   player.position = Vector2.ZERO
   
   var lvl = levels[currLevel].instantiate()
   
-  for i in lvl.get_children() :
-    if "cookie" in i.get_groups() and i is Cookie :
+  for i in lvl.get_children():
+    if "cookie" in i.get_groups() and i is Cookie:
       i.connect("next_level", _nextLevel)
   
-  for k in addTo.get_children() :
+  for k in addTo.get_children():
     addTo.remove_child(k)
   
   var root = get_tree().get_root()
-  for i in root.get_children() :
-    if i is StaticBody2D and "bodies" in i.get_groups() :
+  for i in root.get_children():
+    if i is StaticBody2D and "bodies" in i.get_groups():
       root.remove_child(i)
   
   addTo.add_child(lvl)
@@ -37,10 +38,11 @@ func _loadLevel() :
 func _ready() -> void:
   var root = get_tree().get_root()
   
-  for i in root.get_children() :
-    if "levelcontainer" in i.get_groups() :
+  for i in root.get_children():
+    if "levelcontainer" in i.get_groups():
       addTo = i
       
   currLevel = startingLevel
+  Globals.setCurrentLevel(currLevel)
   
   _loadLevel()
