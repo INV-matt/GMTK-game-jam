5f1196b75b7685be891acde033a059ab17a12e45
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2025-08-01 23:13:04 +0200

    Added mini power death ability

--- Powers/Mini Power/mini_power.gd
+++ Powers/Mini Power/mini_power.gd
@@ -8,3 +8,12 @@ func _power_passive(p: Player) :
 
 func _exit_tree() -> void:
   player.scale *= 2
+
+var shroomScene = preload("res://Powers/Mini Power/mini_shroom.tscn")
+
+func _power_death(p: Player) :
+  var shroom = shroomScene.instantiate()
+  
+  shroom.global_position = p.global_position
+  
+  get_tree().get_root().call_deferred("add_child", shroom)
