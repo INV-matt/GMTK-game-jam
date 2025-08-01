d67d4e8fb9f95e635be3197122aae56b5bbb77fc
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2025-08-01 17:07:59 +0200

    Added passive power for float power

--- Powers/Double Jump Power/double_jump.gd
+++ Powers/Double Jump Power/double_jump.gd
@@ -1,5 +1,11 @@
 extends Power
 
+var p: Player
+
 # Maybe this should be just "= 2" instead of "+ 1" idk
 func _power_passive(player: Player) :
   player.MaxJumps += 1
+  p = player
+
+func _exit_tree() -> void:
+  p.MaxJumps -= 1
