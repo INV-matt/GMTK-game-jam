f41c4bdceed8d18f8150e1435d7c0c912281c167
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2025-08-01 10:28:55 +0200

    Added functionality to select 2 powers with one having the active ability enabled and the other having the on death ability enabled

--- UI/power_select.gd
+++ UI/power_select.gd
@@ -4,6 +4,17 @@ var slotsFull: Array[bool] = []
 var slotsBtn: Array[TextureButton] = []
 
 @export var BlankSlot: CompressedTexture2D
+@export var TextureToPower: Dictionary[CompressedTexture2D, PackedScene]
+
+var TextureToPowername: Dictionary[CompressedTexture2D, String]
+
+func _getPowerNameFromTexture(texture: CompressedTexture2D) :
+  if texture in TextureToPowername :
+    return TextureToPowername[texture]
+  
+  TextureToPowername[texture] = TextureToPower[texture].instantiate().PowerName
+
+  return TextureToPowername[texture]
 
 func _powerSelected(btn: TextureButton) :
   for i in range(len(slotsFull)) :
@@ -12,6 +23,11 @@ func _powerSelected(btn: TextureButton) :
       
       slotsFull[i] = true
       slot.texture_normal = btn.texture_normal
+      
+      if i == 0 :
+        (slot.get_child(0) as RichTextLabel).text = "[center]Active ability: " + _getPowerNameFromTexture(btn.texture_normal) + "[/center]"
+      else :
+        (slot.get_child(0) as RichTextLabel).text = "[center]On death ability: " + _getPowerNameFromTexture(btn.texture_normal) + "[/center]"
       
       return
 
@@ -22,6 +38,11 @@ func _powerUnselected(btn: TextureButton) :
       
       btn.texture_normal = BlankSlot
       
+      if i == 0 :
+        (btn.get_child(0) as RichTextLabel).text = "[center]Active ability: None[/center]"
+      else :
+        (btn.get_child(0) as RichTextLabel).text = "[center]On death ability: None[/center]"
+      
       return
 
 func _ready() -> void:
@@ -31,6 +52,7 @@ func _ready() -> void:
   for i in availible.get_children() :
     if i is TextureButton :
       i.connect("pressed", _powerSelected.bind(i))
+      (i.get_child(0) as RichTextLabel).text = "[center]" + _getPowerNameFromTexture(i.texture_normal) + "[/center]"
   
   for i in slots.get_children() :
     if i is TextureButton :
@@ -40,3 +62,24 @@ func _ready() -> void:
       
       slotsFull.push_back(false)
       slotsBtn.push_back(i)
+  
+  (slotsBtn[0].get_child(0) as RichTextLabel).text = "[center]Active ability: None[/center]"
+  (slotsBtn[1].get_child(0) as RichTextLabel).text = "[center]On death ability: None[/center]"
+
+func _on_select_powers_pressed() -> void:
+  for i in slotsFull :
+    if !i :
+      print("Some slots empty")
+      return
+
+  var activePassive = slotsBtn[0]
+  var activeOnDeath = slotsBtn[1]
+  
+  var powerPassive: Power = TextureToPower[activePassive.texture_normal].instantiate()
+  var powerOnDeath: Power = TextureToPower[activeOnDeath.texture_normal].instantiate()
+
+  powerPassive.OnDeathEnabled = false # Disable ondeath effect
+  powerOnDeath.PassiveEnabled = false # Disable passive effect
+  
+  print(powerPassive)
+  print(powerOnDeath)
