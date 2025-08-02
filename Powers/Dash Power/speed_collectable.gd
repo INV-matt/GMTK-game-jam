05c760443d4100174d94b9848fdb0a40d408bcea
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2025-08-02 21:18:32 +0200

    Added end scene

--- Powers/Dash Power/speed_collectable.gd
+++ Powers/Dash Power/speed_collectable.gd
@@ -25,6 +25,9 @@ func _on_body_exited(body: Node2D) -> void:
   canCollect = true
 
 func _on_timer_timeout() -> void:
+  if !player :
+    return
+  
   player.Speed /= SpeedMult
   player.Acceleration /= SpeedMult
   queue_free()
