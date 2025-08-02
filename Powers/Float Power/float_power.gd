9fc30ededafddf929607a066eb45a4849819c3ca
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2025-08-02 13:51:09 +0200

    Added on-death ability for float power

--- Powers/Float Power/float_power.gd
+++ Powers/Float Power/float_power.gd
@@ -14,5 +14,15 @@ func _process(delta: float) -> void:
   if player.GlobalGravityMult == 1 :
     player.GlobalGravityMult = .5
 
+var boardScene = preload("res://Powers/Float Power/surfboard.tscn")
+
+func _power_death(p: Player) :
+  var board: CharacterBody2D = boardScene.instantiate()
+  
+  board.global_position = p.global_position
+  board.position.y -= 50
+  
+  get_tree().get_root().call_deferred("add_child", board)
+
 func _exit_tree() :
   player.GlobalGravityMult = 1
