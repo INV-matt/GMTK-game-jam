aa39b84dc5f05a413bd1804c2f1a50e354eaca2f
INV-matt <93540623+INV-matt@users.noreply.github.com>
2025-08-03 11:30:27 +0200

    Revert "Revert "More levels""

--- Powers/Gravity Control Power/gravity_control_power.gd
+++ Powers/Gravity Control Power/gravity_control_power.gd
@@ -3,20 +3,20 @@ extends Power
 var isActive = false
 var player: Player
 
-func _power_passive(p: Player) :
+func _power_passive(p: Player):
   isActive = true
-  player = p 
+  player = p
  
 func _process(delta: float) -> void:
-  if !isActive :
+  if !isActive:
     return
   
-  if Input.is_action_just_pressed("pl_attack") :
+  if Input.is_action_just_pressed("pl_attack"):
     player.GlobalGravityDir = -player.GlobalGravityDir
 
 var antiScene = preload("res://Powers/Gravity Control Power/antigravity_area.tscn")
 
-func _power_death(p: Player) :
+func _power_death(p: Player):
   var anti: Area2D = antiScene.instantiate()
   
   anti.global_position = p.global_position
